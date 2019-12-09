6a2d9e530cb0255a4ee41152d192f66875d282fe
Rahavi Ramesh <RRamesh@spartaglobal.com>
2019-12-09 16:53:37 +0000

    Updated README.md with open() and with() function

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Topics covered:
 - Providing adequate feedback while failing with grace
 - When you handle your error, your code can continue to run
 ### Definitions 
-### try: / except:
+### try: / except: / finally:
 - These blocks of code are used in conjunction to error handling 
 
 ````
@@ -24,8 +24,17 @@ except <exception> as <placeholder>:
     block of code
     block of code
     print <placeholder>
+finally:
+    block that runs after 
 
 ````
+### Using open() and with()
+- When using open() you need to close the files you actually open
+- you can skip this step using with()
+   ```` 
+   open(<file>, <option>) as <placeholder>:
+        <placeholder>.readlines()
+    ````
 #### Exceptions
 - These occur when an error occurs 
 
--- orders.py
+++ orders.py
@@ -13,6 +13,7 @@ def open_print_txt_file(file_name):
         print(errmssg)  # Printing the orignal error message
         # raise   #--> raising the error
 
+# Closing the file automatically
 def open_print_close(file_name):
     try:
         with open(file_name, 'r') as file:    # With will automatically open and close a file
@@ -27,8 +28,24 @@ def open_print_close(file_name):
     finally:
         print('////execution done!////')
 
+# Appending the txt file
+def open_and_write_txt(file, item):
+    try:
+        with open(file, 'a') as file_to_write:
+            file_to_write.write(item + '\n')
 
-# open_print_txt_file('order.txt')
+    except FileNotFoundError as error:
+        print('Check your file and the path')
+        print(error)
+    finally:
+        print('////execution done!////')
+
+open_and_write_txt('order2.txt', 'cupcake')
+open_and_write_txt('order2.txt', 'OJ with carrots')
+open_and_write_txt('order2.txt', 'Benedict Beans')
+open_and_write_txt('order2.txt', 'Beans on toast' )
+
+# open_print_txt_file '
 # open_print_txt_file('order2.txt')
 open_print_close('order2.txt')
 
